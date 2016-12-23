0f2aa1d50b70a4bed0027db0d2e8d3bdc0c44096
sion_wang <sion_wang@gemteks.com>
2016-12-23 17:21:56 +0800

    Test onTouch onLongClick onClick.

--- app/src/main/java/com/sion/myfirstrepository/MainActivity.java
+++ app/src/main/java/com/sion/myfirstrepository/MainActivity.java
@@ -1,13 +1,49 @@
 package com.sion.myfirstrepository;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends BaseActivity
 {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+                        Log.d(TAG, "onTouch Action Down");
+                        return false;
+                    case MotionEvent.ACTION_UP:
+                        Log.d(TAG, "onTouch Action Up");
+                        return false;
+                }
+                return false;
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick");
+            }
+        });
+
+        button.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                Log.d(TAG, "onLongClick");
+                return false;
+            }
+        });
     }
 }
