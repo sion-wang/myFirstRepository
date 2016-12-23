da97c54931db2af1119f700d4486a9b3f2fe7400
sion_wang <sion_wang@gemteks.com>
2016-12-23 16:19:35 +0800

    Changed to AppCompatActivity.

---
+++ app/src/main/java/com/sion/myfirstrepository/BaseActivity.java
@@ -0,0 +1,11 @@
+package com.sion.myfirstrepository;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Sion Wang on 2016/12/23.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+}
